f526837e26b51fc5fe73f5698c93bbe764f16685
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-07-01 17:09:39 +0700

    add dialog create transaction

--- fontend/wallet/src/views/User.vue
+++ fontend/wallet/src/views/User.vue
@@ -9,13 +9,68 @@
             color="primary"
             @click="reload">
             Reload
-            
         </v-btn>
         <v-btn
-            depressed
-            color="primary">
-            Create new transaction
+            color="primary"
+            dark
+            @click.stop="dialog = true"
+            >
+            Open Dialog
         </v-btn>
+        <v-dialog
+            v-model="dialog"
+            persistent
+            @click:outside="closeDialog"
+            max-width="600px"
+        >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Create transaction</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-form v-model="formValid" ref="myForm">
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Address"
+                  v-model="address"
+                  :rules="rules.required"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Amount"
+                  type="number"
+                  min="1"
+                  v-model="amount"
+                  :rules="rules.required"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            </v-form>
+            <div v-if="formValid == false && onclick == true" style="color:red">  Data not valid</div>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="closeDialog"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="validateDialog"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
         </v-card-actions>
         <v-row no-gutters>
             <v-tabs fixed-tabs>
@@ -50,10 +105,18 @@ export default {
     name: "UserDetail",
     data() {
         return {
+            formValid: true,
+            dialog: false,
             privateKey: "",
             publicKey: "",
             blockChain: [],
             unspentTx: [],
+            address: "",
+            amount: "",
+            onclick: false,
+            rules: {
+                required: [value => !!value || "Required."]
+            }
         };
     },
     created() {
@@ -86,6 +149,16 @@ export default {
             this.getUnspentTx().then((result) => {
                 this.unspentTx = result.data;
             });
+        },
+        closeDialog(){
+            this.dialog = false;
+            this.onclick = false;
+            this.$refs.myForm.reset()
+        },
+        validateDialog(){
+            this.onclick = true;
+            console.log(this.address)
+            if (this.formValid == true) this.closeDialog()
         }
     },
     components: { MyTrading, AllTrading }
